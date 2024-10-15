df186c5317abcb78464b577dc0c372422ad866d5
z3ZrnSOLANA <166351385+z3ZnSOLANA@users.noreply.github.com>
2024-10-15 18:58:57 +0700

    Update features.py

--- utils/features.py
+++ utils/features.py
@@ -1,6 +1,6 @@
 from construct import Bytes, Int8ul, Int64ul, BytesInteger
 from construct import Struct as cStruct 
-
+ 
 POOL_INFO_LAYOUT = cStruct(
     "instruction" / Int8ul, 
     "simulate_type" / Int8ul
