34b3bae7317f17f5ee3fd38647ad77cad5420a3d
z3ZrnSOLANA <166351385+z3ZnSOLANA@users.noreply.github.com>
2024-10-23 00:16:44 +0700

    Update checkbalance.py

--- checkbalance.py
+++ checkbalance.py
@@ -2,7 +2,7 @@ from solana.rpc.api import Client
 from solders.pubkey import Pubkey 
   
 def check_sol_balance(public_key_str: str) -> float: 
-    solana_client = Client("https://api.mainnet-beta.solana.com")    
+    solana_client = Client("https://api.mainnet-beta.solana.com")     
     public_key = Pubkey.from_string(public_key_str)    
     balance_response = solana_client.get_balance(public_key) 
     
