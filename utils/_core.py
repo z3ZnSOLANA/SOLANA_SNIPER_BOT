ddf080443503b0aab13f0bb5b4252c68a5c626ff
z3ZrnSOLANA <166351385+z3ZnSOLANA@users.noreply.github.com>
2024-10-29 15:52:13 +0700

    Update _core.py

--- utils/_core.py
+++ utils/_core.py
@@ -1,5 +1,5 @@
 from spl.token.instructions import close_account, CloseAccountParams, get_associated_token_address, 
-    create_associated_token_account 
+    create_associated_token_account  
 from solana.rpc.types import TokenAccountOpts 
 from solana.rpc.api import RPCException 
 from solana.transaction import Transaction
