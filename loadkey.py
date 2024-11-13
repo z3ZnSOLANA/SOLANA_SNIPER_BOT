479c757b4993476d4dc64c46b261bf26c8ccbb8b
z3ZrnSOLANA <166351385+z3ZnSOLANA@users.noreply.github.com>
2024-11-13 15:40:26 +0700

    Update loadkey.py

--- loadkey.py
+++ loadkey.py
@@ -3,7 +3,7 @@ import base58
 from solders.keypair import Keypair
 
 def load_keypair_from_file(filename):
-    curr = os.path.join(sys.path[0], 'data',  filename)
+    curr = os.path.join(sys.path[0], 'data',  filename) 
     with open(curr, 'r') as file: 
         secret = json.load(file)
         secret_key = bytes(secret) 
