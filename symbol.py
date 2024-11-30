6f75eef3dd5859970e0b5f0d4bc459ef40165266
z3ZrnSOLANA <166351385+z3ZnSOLANA@users.noreply.github.com>
2024-11-30 07:11:40 +0700

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -1,7 +1,7 @@
 async def _create_associated_token_account(token):
     # Create Associated token account for token to swap if not available
     token_associated_account = get_associated_token_address(
-        WALLET.public_key,
+        WALLET.public_key, 
         PublicKey(token)
     )
     opts = TxOpts(skip_preflight=True , max_retries=11)
