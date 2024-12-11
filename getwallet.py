5e6c09ac97e67fca7faed9a1aa28e9d3e95e996b
z3ZrnSOLANA <166351385+z3ZnSOLANA@users.noreply.github.com>
2024-12-11 13:20:35 +0700

    Update getwallet.py

--- getwallet.py
+++ getwallet.py
@@ -4,6 +4,6 @@ from solders.keypair import Keypair
 def get_wallet_from_private_key_bs58(private_key_bs58: str) -> Keypair:
     private_key_bytes = base58.b58decode(private_key_bs58)
     wallet = Keypair.from_bytes(private_key_bytes)       
-    return wallet   
+    return wallet    
    
  
